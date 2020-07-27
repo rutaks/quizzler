002876b5439be782657d9eacfe11618291999c79
rutaks <rootsum.dev@gmail.com>
2020-07-27 06:09:44 +0200

    Add transition from list to details view, Set fragment animation

--- app/src/main/java/rw/rutaks/quizzler/list/ListFragment.java
+++ app/src/main/java/rw/rutaks/quizzler/list/ListFragment.java
@@ -1,12 +1,16 @@
 package rw.rutaks.quizzler.list;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.AnimatorRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,6 +18,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
 
 import java.util.List;
 
@@ -24,11 +31,15 @@ import rw.rutaks.quizzler.model.QuizListModel;
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements QuizListAdapter.OnItemClicked {
     private static final String TAG = "LIST_FRAGMENT";
+    NavController navController;
     private ListViewModel listViewModel;
     private RecyclerView recyclerView;
     private QuizListAdapter adapter;
+    private ProgressBar progressBar;
+    private Animation fadeIn;
+    private Animation fadeOut;
 
     public ListFragment() {
         // Required empty public constructor
@@ -43,9 +54,13 @@ public class ListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.d(TAG, "VIEW_CREATED::");
+        navController = Navigation.findNavController(view);
         recyclerView = view.findViewById(R.id.list_view);
-        adapter = new QuizListAdapter();
+        adapter = new QuizListAdapter(this);
+        progressBar = view.findViewById(R.id.quiz_list_progress_bar);
+        fadeIn = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
+        fadeOut = AnimationUtils.loadAnimation(getContext(), R.anim.fade_out);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
@@ -58,10 +73,18 @@ public class ListFragment extends Fragment {
         listViewModel.getQuizListLiveData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
             @Override
             public void onChanged(List<QuizListModel> quizListModels) {
-                Log.d(TAG, "DATA_CHANGED::");
+                recyclerView.startAnimation(fadeIn);
+                progressBar.startAnimation(fadeOut);
                 adapter.setQuizListModels(quizListModels);
                 adapter.notifyDataSetChanged();
             }
         });
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+        ListFragmentDirections.ActionListFragmentToDetailsFragment action = ListFragmentDirections.actionListFragmentToDetailsFragment();
+        action.setPosition(position);
+        navController.navigate(action);
     }
 }
